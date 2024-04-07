d75496e90387d8c241161dae36ae5424a905c9e4
Chiazor Daniel <chiazordaniel317@gmail.com>
2024-04-07 12:52:29 +0100

    dashboard

---
+++ src/src2/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Route from './pages/route';	
+
+import "./assets/css/style.css";
+
+
+function Landing() {
+  return (
+    <div className="App">
+		  <Route />
+    </div>
+  );
+}
+
+export default Landing;
